fa3ec3fae60f985c90b2d078a671d21c3c0bd12a
twinaphex <libretro@gmail.com>
2014-06-22 00:21:10 +0200

    Remove DecodeColor from surface.h

--- mednafen/video/surface.h
+++ mednafen/video/surface.h
@@ -86,25 +86,6 @@ class MDFN_PixelFormat
  {
     return MAKECOLOR(r, g, b, a);
  }
-
- // Gets the R/G/B/A values for the passed 32-bit surface pixel value
-#if defined(WANT_32BPP)
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b, int &a) const
- {
-    r = (value >> RED_SHIFT) & 0xFF;
-    g = (value >> GREEN_SHIFT) & 0xFF;
-    b = (value >> BLUE_SHIFT) & 0xFF;
-    a = (value >> ALPHA_SHIFT) & 0xFF;
- }
-#elif defined(WANT_16BPP)
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b, int &a) const
- {
-    r = (value & BLUE_MASK) << RED_SHIFT;
-    g = (value & GREEN_MASK) << GREEN_SHIFT;
-    b = (value & RED_MASK);
- }
-#endif
-
 }; // MDFN_PixelFormat;
 
 // Supports 32-bit RGBA
@@ -142,40 +123,6 @@ class MDFN_Surface //typedef struct
  {
     return MAKECOLOR(r, g, b, a);
  }
-
-#if defined(WANT_32BPP)
- // Gets the R/G/B/A values for the passed 32-bit surface pixel value
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b, int &a) const
- {
-    r = (value >> RED_SHIFT) & 0xFF;
-    g = (value >> GREEN_SHIFT) & 0xFF;
-    b = (value >> BLUE_SHIFT) & 0xFF;
-    a = (value >> ALPHA_SHIFT) & 0xFF;
- }
-
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b) const
- {
-    r = (value >> RED_SHIFT) & 0xFF;
-    g = (value >> GREEN_SHIFT) & 0xFF;
-    b = (value >> BLUE_SHIFT) & 0xFF;
- }
-#elif defined(WANT_16BPP)
-
- // Gets the R/G/B/A values for the passed 32-bit surface pixel value
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b, int &a) const
- {
-    r = (value & BLUE_MASK) << RED_SHIFT;
-    g = (value & GREEN_MASK) << GREEN_SHIFT;
-    b = (value & RED_MASK);
- }
-
- INLINE void DecodeColor(uint32 value, int &r, int &g, int &b) const
- {
-  r = (value & BLUE_MASK) << RED_SHIFT;
-  g = (value & GREEN_MASK) << GREEN_SHIFT;
-  b = (value & RED_MASK);
- }
-#endif
  private:
  void Init(void *const p_pixels, const uint32 p_width, const uint32 p_height, const uint32 p_pitchinpix, const MDFN_PixelFormat &nf);
 };
