cac2ff72f16631e32947a8a36bf2bc328e6d8523
twinaphex <libretro@gmail.com>
2015-07-23 17:43:40 +0200

    Cleanups

--- mednafen/mednafen-driver.h
+++ mednafen/mednafen-driver.h
@@ -22,26 +22,6 @@ void MDFND_Message(const char *s);
 uint32 MDFND_GetTime(void);
 void MDFND_Sleep(uint32 ms);
 
-#ifdef WANT_THREADING
-/* Being threading support. */
-// Mostly based off SDL's prototypes and semantics.
-// Driver code should actually define MDFN_Thread and MDFN_Mutex.
-
-struct MDFN_Thread;
-struct MDFN_Mutex;
-
-MDFN_Thread *MDFND_CreateThread(int (*fn)(void *), void *data);
-void MDFND_WaitThread(MDFN_Thread *thread, int *status);
-void MDFND_KillThread(MDFN_Thread *thread);
-
-MDFN_Mutex *MDFND_CreateMutex(void);
-void MDFND_DestroyMutex(MDFN_Mutex *mutex);
-int MDFND_LockMutex(MDFN_Mutex *mutex);
-int MDFND_UnlockMutex(MDFN_Mutex *mutex);
-
-/* End threading support. */
-#endif
-
 /* path = path of game/file to load.  returns NULL on failure. */
 MDFNGI *MDFNI_LoadGame(const char *force_module, const char *path);
 
