87fda219566af1e3917a813d6fd49a5688cb0da9
twinaphex <libretro@gmail.com>
2016-08-26 17:15:00 +0200

    Backport this from 0.9.39.1 - 'Fixed a bad usage of "char" without an unsigned/signed qualifier

--- mednafen/wswan/gfx.cpp
+++ mednafen/wswan/gfx.cpp
@@ -356,7 +356,6 @@ void WSwan_SetPixelFormat(void)
 void wsScanline(uint16 *target)
 {
    uint32		start_tile_n,map_a,startindex,adrbuf,b1,b2,j,t,l;
-   char		ys2;
    uint8		b_bg[256];
    uint8		b_bg_pal[256];
 
@@ -515,13 +514,12 @@ void wsScanline(uint16 *target)
          ts = SpriteTable[h][0];
          as = SpriteTable[h][1];
          ysx = SpriteTable[h][2];
-         ys2 = (int8)SpriteTable[h][2];
          xs = SpriteTable[h][3];
 
          if(xs >= 249) xs -= 256;
 
          if(ysx > 150) 
-            ys = ys2;
+            ys = (int8)ysx;
          else 
             ys = ysx;
 
