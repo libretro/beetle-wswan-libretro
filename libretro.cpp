3962ae38f078eda862fdaa0abed8e8754a818579
Hedonium <devmohmoud@gmail.com>
2018-02-19 21:32:04 -0500

    Update libretro.cpp: Add input descriptor
    The Rotate screen and the active D-Pad input mapped to the Select button was missing a descriptor.
    
    Compiled and tested successfully on Windows 10 x64.

--- libretro.cpp
+++ libretro.cpp
@@ -569,6 +569,7 @@ bool retro_load_game(const struct retro_game_info *info)
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A, "A" },
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B, "B" },
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START, "Start" },
+      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT, "Rotate screen + active D-Pad" },
 
       { 0 },
    };
