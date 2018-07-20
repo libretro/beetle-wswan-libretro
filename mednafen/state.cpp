b4dc85b0ada2b27af3d4420439b0e4528363ef67
twinaphex <libretro@gmail.com>
2018-07-20 13:31:30 +0200

    Buildfix for MSVC 2005

--- mednafen/state.cpp
+++ mednafen/state.cpp
@@ -19,11 +19,10 @@
 
 #include <boolean.h>
 
-#include <compat/msvc.h>
-
 #include "mednafen.h"
 #include "driver.h"
 #include "state.h"
+#include <compat/msvc.h>
 
 #define RLSB 		MDFNSTATE_RLSB	//0x80000000
 
