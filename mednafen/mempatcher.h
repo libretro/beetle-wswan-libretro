6e2262499f37daaf1d3cb2cd503cfbb9870f8cc5
twinaphex <libretro@gmail.com>
2020-09-23 02:46:51 +0200

    Cleanups

--- mednafen/mempatcher.h
+++ mednafen/mempatcher.h
@@ -2,7 +2,6 @@
 #define __MDFN_MEMPATCHER_H
 
 #include "mempatcher-driver.h"
-#include <vector>
 
 typedef struct __SUBCHEAT
 {
@@ -11,13 +10,11 @@ typedef struct __SUBCHEAT
 	int compare; // < 0 on no compare
 } SUBCHEAT;
 
-extern std::vector<SUBCHEAT> SubCheats[8];
 extern bool SubCheatsOn;
 
 bool MDFNMP_Init(uint32 ps, uint32 numpages);
 void MDFNMP_AddRAM(uint32 size, uint32 address, uint8 *RAM);
 void MDFNMP_Kill(void);
-
 
 void MDFNMP_InstallReadPatches(void);
 void MDFNMP_RemoveReadPatches(void);
